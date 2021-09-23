ae86a9c5075d2cd609b689a75fa6e1ed2548428e
Khailas R <khailas303@gmail.com>
2021-09-24 00:20:56 +0530

    initial commit

---
+++ face_detector.py
@@ -0,0 +1,84 @@
+import mediapipe as mp
+import concurrent.futures
+import cv2
+import time
+
+
+
+class FaceDetector:
+    def __init__(self):
+        self.mp_face_detector = None
+        self.drawing = None
+
+    def __enter__(self): 
+        print('__enter__')
+        return self
+
+    def my_face_detection(self):
+        
+        self.mp_face_detector = mp.solutions.face_detection
+        self.mp_drawing = mp.solutions.drawing_utils
+        
+        previous_time = 0
+        current_time = 0
+        
+        cam = cv2.VideoCapture(0)
+        
+        with self.mp_face_detector.FaceDetection(
+            model_selection=0, min_detection_confidence=0.5
+            ) as face_detection:
+            
+            while cam.isOpened():
+                success, camera = cam.read()
+                
+                if not success:
+                    print('ignoring empty camera frame')
+                    continue
+                
+                self.image_rgb = cv2.cvtColor(cv2.flip(camera, 1), cv2.COLOR_BGR2RGB)
+                
+                camera.flags.writeable = False
+                self.results = face_detection.process(camera)
+                
+                
+                camera.flags.writeable = True
+                camera = cv2.cvtColor(camera, cv2.COLOR_RGB2BGR)
+                
+                if self.results.detections:
+                    for detection in self.results.detections:
+                        self.mp_drawing.draw_detection(camera, detection)
+                        
+                    cv2.imshow('face detector', camera)
+                    
+                    if cv2.waitKey(1) & 0xFF==ord("q"):
+                        break
+                    
+        cam.release()
+        
+        
+    def camera_position(self, camera, face_num=0, draw=True):
+        land_mark_list = []
+        if self.results.detections:
+            my_camera = self.results.detections[face_num]
+            
+            for id, lm in enumerate(my_camera.landmark):
+                height, width, channels = camera.shape
+                
+                cx, cy = int(lm.x*width), int(lm.y*height)
+                land_mark_list.append([id, cx, cy])
+                
+            if draw:
+                cv2.circle(camera, (cx, cy), 15, (255, 0, 255),cv2.FILLED)
+        return land_mark_list    
+    
+    def __exit__(self, type, value, traceback):
+        return isinstance(value, TypeError)
+    
+        
+                  
+if __name__ == "__main__":
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        
+        with FaceDetector() as fd:
+            executor.map(fd.my_face_detection())
+            executor.map(fd.camera_position())
---
+++ walkthrough_docs.py
@@ -0,0 +1,70 @@
+import mediapipe as mp
+import cv2
+
+
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+
+
+image_files = []
+
+with mp_face_detection.FaceDetection(
+    model_selection=1, min_detection_confidence=0.5) as face_detection:
+
+    for idx, file in enumerate(image_files):
+        image = cv2.imread(file)
+        
+        results = face_detection.process(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))    # converting BGR to RGB
+        
+        
+        if not results.detections:
+            continue
+        
+        annotated_image = image.copy()                                 
+        for detection in results.detections:
+            print('Nose tip')
+
+        print(mp_face_detection.get_key_point(
+                detection, mp_face_detection.FaceKeyPoint.NOSE_TIP
+            ))
+        
+        mp_drawing.draw_detection(annotated_image, detection)
+        cv2.imwrite('tmp/annotated_image' + str(idx) + '.png', annotated_image)
+        
+        
+
+# webcam input
+cap =  cv2.VideoCapture(0)
+
+with mp_face_detection.FaceDetection(
+    model_selection=0, min_detection_confidence=0.5
+) as face_detection:
+    
+    while cap.isOpened():
+        success, image = cap.read()
+        
+        if not success:
+            print('ignoring empty camera frame')
+            continue
+        
+        
+        # flip img horizontally and converting BGR img to RGB
+        img = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = face_detection.process(image)
+        
+        
+        # drawing the face detection annotations 
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        if results.detections:
+            for detection in results.detections:
+                mp_drawing.draw_detection(image, detection)
+                
+            cv2.imshow('face detector', image)
+            
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+            
+cap.release()            
