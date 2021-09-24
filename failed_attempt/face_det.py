8d3382d3f63712c50f44e0db4624104639633b40
Khailas R <khailas303@gmail.com>
2021-09-24 19:43:54 +0530

    updated

--- failed_attempt/face_det.py
+++ failed_attempt/face_det.py
@@ -1,16 +1,16 @@
 import mediapipe as mp
-import concurrent.futures
 import cv2
-import time
-
 
 
 class FaceDetector():
 
     def __init__(
-        self, static_image_mode = False, min_detection_confidence=0.5, min_tracking_confidence=0.):
+        self, static_image_mode = False, max_faces=2,
+        min_detection_confidence=0.5, min_tracking_confidence=0.):
+        
         
         self.static_image_mode = static_image_mode
+        self.max_faces = max_faces
         self.min_detection_confidence = min_detection_confidence
         self.min_tracking_confidence = min_tracking_confidence
         self.mp_face_detector = None
@@ -20,25 +20,34 @@ class FaceDetector():
         self.mp_face_detector = mp.solutions.face_detection
         
         self.detector = self.mp_face_detector.FaceDetection(
-            self.static_image_mode, self.min_detection_confidence, self.min_tracking_confidence 
+            self.static_image_mode, 
+            self.max_faces,
+            self.min_detection_confidence, 
+            self.min_tracking_confidence 
         )
         
         self.mp_drawing = mp.solutions.drawing_utils
+        self.drawSpec = self.mp_drawing.DrawingSpec(thickness=1, circle_radius=2)
         
         
     def find_face(self, camera, draw=True):    
-        camera_rgb = cv2.cvtColor(cv2.flip(camera, 1), cv2.COLOR_BGR2RGB)
+        self.camera_rgb = cv2.cvtColor(cv2.flip(camera, 1), cv2.COLOR_BGR2RGB)
         
         camera.flags.writeable = False
-        self.results = self.detector.process(camera)
+        self.results = self.detector.process(self.camera_rgb)
         
         camera.flags.writeable = True
         camera = cv2.cvtColor(camera, cv2.COLOR_RGB2BGR)
         
-        if self.results.detections:
-            for detection in self.results.detections:
+        
+        if self.results.multi_face_landmarks:
+            for detection in self.results.multi_face_landmarks:
                 if draw:
-                    self.mp_drawing.mp_drawing(camera, self.mp_face_detector, detection)
+                    self.mp_drawing.draw_landmarks(
+                        camera, self.mp_face_detector.FACE_CONNECTIONS,
+                        detection, self.drawSpec, self.drawSpec
+                        )
+                face = []
                         
         return camera
     
@@ -46,8 +55,8 @@ class FaceDetector():
     def camera_position(self, camera, face_num=0, draw=True):
         land_mark_list = []
     
-        if self.results.detections:
-            my_camera = self.results.detections[face_num]
+        if self.results.multi_face_landmarks:
+            my_camera = self.results.multi_face_landmarks[face_num]
             
             for id, lm in enumerate(my_camera.landmark):
                 height, width, channels = camera.shape
